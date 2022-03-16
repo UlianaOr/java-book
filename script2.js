1b8143ee4c3f62445e4214daf06ae7af95bf2ba1
UlianaOr <Ouliana9@gmail.com>
2022-03-16 17:31:28 +0100

    blAreas

---
+++ script2.js
@@ -0,0 +1,11 @@
+    /* Block Areas*/
+
+    let safeToProceed = false;
+function isItSafe() {
+ if (safeToProceed) {
+ alert("You shall pass!");
+ } else {
+ alert("You shall not pass!");
+ }
+}
+isItSafe();
