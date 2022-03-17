3c27c72cbf3819212667f6494a8ec015647cf13b
UlianaOr <Ouliana9@gmail.com>
2022-03-17 12:04:23 +0100

    FF

--- script2.js
+++ script2.js
@@ -1,6 +1,13 @@
-    /* Block Areas*/
+    /* Block Areas
+    Место размещения переменных в коде имеет огромное влияние на то, где 
+они могут использоваться. Переменные, объявленные глобально, доступны 
+по всему приложению. Переменные, объявленные локально, — только 
+внутри области, в которой расположены. Внутри диапазона глобальных 
+и локальных переменных у JavaScript есть огромное количество действий 
+в запасе.*/
 
-    let safeToProceed = false;
+
+    /*let safeToProceed = false;
 function isItSafe() {
  if (safeToProceed) {
  alert("You shall pass!");
@@ -8,4 +15,17 @@ function isItSafe() {
  alert("You shall not pass!");
  }
 }
-isItSafe();
+isItSafe();*/
+
+
+
+   //Functions within functions
+   function youSayGoodBye() {
+    alert("Good Bye!");
+    function andISayHello() {
+    alert("Hello!");
+    }
+    return andISayHello;
+   } 
+
+
